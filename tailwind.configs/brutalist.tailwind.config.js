b1f1f26e5a4d9ac3cea5b53cf06954b723531898
Amaan Ali <amaanali2002@hotmail.com>
2023-01-30 14:10:21 -0500

    divides the tailwind.config files by theme

--- tailwind.configs/brutalist.tailwind.config.js
+++ tailwind.configs/brutalist.tailwind.config.js
@@ -1,8 +1,14 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
     ],
-    presets: [require("./tailwind.configs/elegant.tailwind.config.js")],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
